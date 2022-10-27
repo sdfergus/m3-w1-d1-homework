e6dc8e43fb97f7f9816a5f5a049fee4f2921584b
Shandira Ferguson <shandira.ferguson@gmail.com>
2022-10-27 15:09:49 -0700

    Task 1 complete

--- task1-http/server_task1.js
+++ task1-http/server_task1.js
@@ -4,10 +4,28 @@ var hostname = 'localhost';
 var port = 5000;
 
 var server = http.createServer((req, res) => {
-    console.log('req.headers: ', req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Home Page.</h1></body></html>');
+    // console.log('req.headers: ', req.headers);
+    var urlRequest = req.url;
+
+    if(req.method === 'GET') {
+        if(urlRequest === '/') {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<html><body><h1>Home Page.</h1></body></html>');
+        } else if(urlRequest === '/about') {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<html><body><h1>About Page.</h1></body></html>');
+        } else if(urlRequest === '/contact') {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<html><body><h1>Contact Page.</h1></body></html>');            
+        } else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<html><body><h1>Invalid Request!</h1></body></html>');
+        }
+    }
 });
 
 server.listen(port, hostname, () => {
